15cd862dc927dc5181144db72064929de16f60a0
Manikandan610 <manikandan.jn@fibonalabs.com>
2022-05-27 19:00:11 +0530

    cards Done

--- main.js
+++ main.js
@@ -3,13 +3,20 @@ $(document).ready(function(e){
 
 });
 
+// function example(){
+//     jsonObject.products.forEach((i)=>loadProducts(i));
+
+// }
 function loadProducts(data){
-    '<div class="card2">'+
-    '<div class="rect1"><img src="'+data.image +'" class="rect_img"></div>'+
+
+var data1 = document.createElement('div');
+   data1.innerHTML =  '<div class="card2">'+
+    '<div class="rect1"><img src="'+ data.image +'" class="rect_img"></div>'+
     '<div class="subrect"><p class="subrect_p1">'+ data.title +'</p>'+
     '<span>'+ data.description +'</span>'+
     '<p class="subrect_p2">'+ data.content +'</p>'+
 '</div>';
+document.getElementById("content1").appendChild(data1);
 //console.log(data);
-//document.getElementById('content').append;
 }
+// example();
