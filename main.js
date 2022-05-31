62830c8b264a42d515336b6a898976a8dbb2d1fe
Manikandan610 <manikandan.jn@fibonalabs.com>
2022-05-31 19:14:04 +0530

    Hover Updated

--- main.js
+++ main.js
@@ -5,29 +5,77 @@ $(document).ready(function(e){
 
 // function example(){
 //     jsonObject.products.forEach((i)=>loadProducts(i));
-
+// onmouseover="bigImg(this)" onmouseout="normalImg(this)"
 // }
 function loadProducts(data){
    
 var data1 = document.createElement('div');
-   data1.innerHTML =  '<div class="card2" onmouseover="bigImg(this)" onmouseout="normalImg(this)">'+
+   data1.innerHTML =  '<div class="card2" id="card2">'+
     '<div class="rect1"><img src="'+ data.image +'" class="rect_img"></div>'+
         '<div class="subrect"><p class="subrect_p1">'+ data.title +'</p>'+
         '<span>'+ data.description +'</span>'+
         '<p class="subrect_p2">'+ data.content +'</p>'+
-    '</div>'+
-    '<div class="card1">'+
-            '<p class="card1_p">This workflow is to enable an employee raise his leave request and get it approved it from him reporting manager.</p>'+
-            '<button type="button" class="btn btn-primary btn-sm" id="btn">View Details</button>'+
-        '</div>';
-document.getElementById("content1").appendChild(data1);
+    '</div>';
+    
+
 //console.log(data);
+
+                data1.addEventListener("mouseover", function(e){
+                    //alert("hai");
+                    document.getElementById("card2").style.display="none";
+                    data1.innerHTML = '<div class="card1" style="display:block;">'+
+                            '<p class="card1_p">'+ data.view_details + '</p>'+
+                            '<button type="button" class="btn btn-primary btn-sm" id="btn">View Details</button>'+
+                        '</div>';
+                        //document.getElementById("content1").appendChild(data1);
+                });
+                data1.addEventListener("mouseout", function(e){
+                    //alert("hai");
+                    document.getElementById("card2").style.display="block";
+                    // data1.innerHTML = '<div class="card1" style="display:none;">'+
+                    //         '<p class="card1_p">'+ data.view_details + '</p>'+
+                    //         '<button type="button" class="btn btn-primary btn-sm" id="btn">View Details</button>'+
+                    //     '</div>';
+                        //document.getElementById("content1").appendChild(data1);
+                });
+                document.getElementById("content1").appendChild(data1);
+
 }
 
-// card2.addEventListener("mouseover",function({
-//    
+// function bigImg(x) {
+//     //document.getElementById(data.e_id).style.display="none";
+//     var data2 = document.getElementById('content1');
 
-// },true))
+//    data2.innerHTML =  '<div class="card2" id="'+x.e_id+'" style="display:none;"></div>'+'<div class="card1" style="display:block;">'+
+//             '<p class="card1_p">'+ x.view_details + '</p>'+
+//             '<button type="button" class="btn btn-primary btn-sm" id="btn">View Details</button>'+
+//         '</div>';
+// document.getElementById("content1").appendChild(data2);
+// }
+
+// function normalImg(x) {
+//     //document.getElementById(data.e_id).style.display="none";
+//     var data2 = document.getElementById('content1');
+
+//    data2.innerHTML =  '<div class="card2" id="'+x.e_id+'" style="display:block;"></div>'+'<div class="card1" style="display:block;">'+
+//             '<p class="card1_p">'+ x.view_details + '</p>'+
+//             '<button type="button" class="btn btn-primary btn-sm" id="btn">View Details</button>'+
+//         '</div>';
+// document.getElementById("content1").appendChild(data2);
+// }
+
+// function myFunction() {
+//     const div1 = document.getElementsByTagName("div");
+//     const nodes = div1.getElementById("card2");
+//     //let i=0;
+//     for (let i = 0; i < nodes.length; i++) {
+//       nodes[i].style.display = "none";
+//     }
+//     // for(let i of nodes){
+//     //     nodes[i].css.style.display = "none";
+//     // }
+// }
+
 
 // example();
 // $(function() {
@@ -40,26 +88,9 @@ document.getElementById("content1").appendChild(data1);
 //       //$('.card2').css('display', 'block');
 //     });
 //   });
-// function bigImg(x) {
-//         $('.rect1').css('display', 'none');
-//         $('.rect_img').css('display', 'none');
-//         $('.subrect').css('display', 'none');
-//         $('.subrect_p1').css('display', 'none');
-//         $('span').css('display', 'none');
-//         $('.subrect_p2').css('display', 'none');
-//         $('.card1').css('display', 'block');
-//         // $('.card1_p1').css('display', 'block');
-//         // $('#btn').css('display', 'block');
-//   }
-  
-//   function normalImg(x) {
-//     $('.rect1').css('display', 'block');
-//     $('.rect_img').css('display', 'block');
-//     $('.subrect').css('display', 'block');
-//     $('.subrect_p1').css('display', 'block');
-//     $('span').css('display', 'block');
-//     $('.subrect_p2').css('display', 'block');
-//     $('.card1').css('display', 'none');
-//   }
+// $(document).ready(function(e){
+//     forEach((i)=>bigImg(i));
+
+// });
 
 
